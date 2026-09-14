a891ca14ecc23093e0cd561d2365c0957fa982b1
canghai0823 <canghai0823@users.noreply.github.com>
2026-09-14 09:55:42 +0800

    refine automatic update log

--- src/service.cpp
+++ src/service.cpp
@@ -301,7 +301,7 @@ void automaticUpdateLoop(std::atomic_bool &stopping,
   while (!stopping.load()) {
     lock.unlock();
     try {
-      std::cerr << "batcan automatic update check\n";
+      std::cerr << "batcan automatic update check started\n";
       const auto result = runCommand({kUpdateScriptPath}, true);
       if (result != 0) {
         std::cerr << "batcan automatic update exited with status "
